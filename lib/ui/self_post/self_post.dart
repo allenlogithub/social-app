e04020383aaa11babca2fd15877202127ca18d23
outlierlo <allenlo0130@gmail.com>
2022-02-08 13:54:44 +0800

    [mod] edit self post screen, add a text input field for a user without any post

--- lib/ui/self_post/self_post.dart
+++ lib/ui/self_post/self_post.dart
@@ -12,6 +12,8 @@ class SelfArticlePost extends StatefulWidget {
 
 class _SelfArticlePostState extends State<SelfArticlePost> {
   late Future<GetSelfArticleResponse> futureGetSelfArticleResponse;
+  final TextEditingController _newSelfArticleContent = TextEditingController();
+  var dropdownValue;
 
   @override
   void initState() {
@@ -26,11 +28,130 @@ class _SelfArticlePostState extends State<SelfArticlePost> {
         future: futureGetSelfArticleResponse,
         builder: (context, snapshot) {
           List<dynamic> arts = snapshot.data?.message ?? [];
+          // if no article found
           if (arts.isEmpty) {
-            return Container(
-                // add sth
-                );
+            return Scaffold(
+              body: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SafeArea(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 20),
+                          Text(
+                            'Start Your Frist Post.',
+                            style: GoogleFonts.lato(
+                                color: Colors.white,
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 20),
+                          Column(
+                            children: <Widget>[
+                              Card(
+                                color: Colors.grey,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    maxLines: 8,
+                                    decoration: const InputDecoration.collapsed(
+                                        hintText: "Enter your post here."),
+                                    controller: _newSelfArticleContent,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Center(
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton<String>(
+                                    value: dropdownValue,
+                                    icon: const Icon(
+                                        Icons.arrow_drop_down_circle),
+                                    elevation: 5,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                    onChanged: (String? newValue) {
+                                      setState(() {
+                                        dropdownValue = newValue!;
+                                      });
+                                    },
+                                    hint: const Text("Scope"),
+                                    items: <String>['Global', 'Self']
+                                        .map<DropdownMenuItem<String>>(
+                                            (String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                              )),
+                              Expanded(
+                                  child: Center(
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      // Future<SelfArticlePostResponse> post = SelfArticlePostRequest(
+
+                                      // ),
+                                    },
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.amber),
+                                      shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(50.0),
+                                          side: const BorderSide(
+                                            color: Colors.amber,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Post',
+                                          style: GoogleFonts.lato(
+                                            fontSize: 20,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                              )),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
           }
+          // if article found
           return ListView.builder(
               itemCount: arts.length,
               itemBuilder: (context, index) {
@@ -55,8 +176,8 @@ class _SelfArticlePostState extends State<SelfArticlePost> {
                             return Card(
                                 child: Column(
                               children: <Widget>[
-                                Text(cmt['userName'].toString()),
-                                Text(cmt['comment'].toString()),
+                                Text(cmt['userName']),
+                                Text(cmt['comment']),
                               ],
                             ));
                           }),
