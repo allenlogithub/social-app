b4e164a7c9c5b949fdd730bc195fd3d260654a35
outlierlo <allenlo0130@gmail.com>
2022-02-09 16:44:30 +0800

    [add] add comment content showing widget

---
+++ lib/widgets/self_post/commentShowing.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CommentShowing extends StatefulWidget {
+  final dynamic article;
+  const CommentShowing({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  @override
+  _CommentShowingState createState() => _CommentShowingState();
+}
+
+class _CommentShowingState extends State<CommentShowing> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.article['items'].length,
+        // physics: ClampingScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (context, cmtIndex) {
+          final cmt = widget.article['items'][cmtIndex];
+          return Card(
+            child: Row(
+              children: <Widget>[
+                const Icon(
+                  Icons.account_box_rounded,
+                  color: Colors.white,
+                  size: 50.0,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      cmt['userName'],
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(cmt['comment'],
+                        style: GoogleFonts.lato(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
+  }
+}
