1c4574b8adcdb4ddc525053941c2b238bc4cfafc
outlierlo <allenlo0130@gmail.com>
2022-02-18 12:26:17 +0800

    [add, mod] add API calling, the network screen is now able to search users

---
+++ lib/network/connection/searchUser.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+
+import 'package:social_app/config/config.dart';
+import 'package:social_app/network/auth/login.dart';
+
+class SearchUserResponse {
+  String err;
+  dynamic message;
+
+  SearchUserResponse({
+    required this.err,
+    required this.message,
+  });
+
+  factory SearchUserResponse.fromJson(Map<String, dynamic> json) =>
+      new SearchUserResponse(
+        err: json["err"] ?? "",
+        message: json["message"]["items"],
+      );
+}
+
+Future<SearchUserResponse> searchUserRequest(String userString) async {
+  Response response;
+  var options = BaseOptions(
+    baseUrl: server_domain['user'],
+    connectTimeout: 500,
+    receiveTimeout: 300,
+  );
+  var dio = Dio(options);
+  dio.interceptors.add(CookieManager(cookieJar));
+  try {
+    response = await dio.get('/v1/user/searchuser?SearchString=' + userString);
+    final jsonData = response.data;
+    SearchUserResponse searchUserResponse =
+        SearchUserResponse.fromJson(Map<String, dynamic>.from(jsonData));
+    print(searchUserResponse.message);
+    return searchUserResponse;
+  } catch (e) {
+    if (e is DioError) {
+      return SearchUserResponse(err: "SearchUserError", message: e.toString());
+    }
+    return SearchUserResponse(
+        err: "UnknownSearchUserError", message: e.toString());
+  }
+}
--- lib/ui/network/network.dart
+++ lib/ui/network/network.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:social_app/widgets/network/myNetwork.dart';
+import 'package:social_app/network/connection/searchUser.dart';
 
 class Network extends StatefulWidget {
   const Network({Key? key}) : super(key: key);
@@ -11,82 +12,141 @@ class Network extends StatefulWidget {
 }
 
 class _NetworkState extends State<Network> {
+  final TextEditingController _searchController = TextEditingController();
+  late Future<SearchUserResponse> futureSearchUserResponse;
+
+  void _searchUser(String s) {
+    if (s.length > 1) {
+      futureSearchUserResponse = searchUserRequest(s);
+    }
+    _searchController.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: () {
-              print("Tapped on My Network");
-              Get.to(() => MyNetwork());
-            },
+      body: GestureDetector(
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: const <Widget>[
-                  SizedBox(
-                    height: 10,
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 20,
                   ),
-                  Text(
-                    "My Network",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white70,
+                        ),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10.0))),
+                    height: 40,
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          //same action as TextField.onSubmitted
+                          icon: const Icon(Icons.search),
+                          iconSize: 30,
+                          onPressed: () {
+                            _searchUser(_searchController.text);
+                          },
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: _searchController,
+                            decoration: const InputDecoration.collapsed(
+                              hintText: " Search connections?",
+                            ),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                            ),
+                            onSubmitted: (value) {
+                              // same action as IconButton
+                              _searchUser(value);
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Spacer(),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 30.0,
+                  const SizedBox(
+                    height: 30,
                   ),
-                  SizedBox(
-                    height: 10,
+                  GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      print("Tapped on My Network");
+                      Get.to(() => MyNetwork());
+                    },
+                    child: Row(
+                      children: const <Widget>[
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "My Network",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 30.0,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      print("Tapped Invitations");
+                    },
+                    child: Row(
+                      children: const <Widget>[
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Invitations",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 30.0,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 1000,
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: () {
-              print("Tapped Invitations");
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: const <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Invitations",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 30.0,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
+          )),
+    );
   }
 }
